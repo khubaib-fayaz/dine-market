297489e5432fb76201adee302410128d55d2c8f7
khubaib-ak-47 <malikahma123d@gmail.com>
2023-05-31 11:47:39 +0500

    change new icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="top-0 w-full max-w-[90%] mx-auto">
-      {/* <Head>
-        <link rel="shortcut icon" href="/favion.ico" />
-      </Head> */}
+      <Head>
+        <link rel="shortcut icon" href="/mini.png" />
+      </Head>
       {/* <Navbar/> */}
     </div>
   );
